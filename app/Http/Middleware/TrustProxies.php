8d20036a79fd35f8526548b57b71e059e1f52972
Votre Sylla <fatoumatbinetousylla@gmail.com>
2025-10-24 14:55:39 +0000

    Remove HTTPS setting in TrustProxies for local development

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -26,11 +26,5 @@ class TrustProxies extends Middleware
         Request::HEADER_X_FORWARDED_PROTO,
         Request::HEADER_X_FORWARDED_AWS_ELB,
     ];
-    public function  handle($request, \Closure $next)
-    {
-        if (app()->environment('production')) {
-     $request->server->set('HTTPS', 'on');
-    }           
-        return parent::handle($request, $next);
-    }
+    
 }
