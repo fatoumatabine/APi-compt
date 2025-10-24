84a15395a9f6ea22294e50b18cb1e7cfc144d49d
Votre Sylla <fatoumatbinetousylla@gmail.com>
2025-10-24 15:45:32 +0000

    Add back HTTPS setting in TrustProxies for production

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -26,5 +26,13 @@ class TrustProxies extends Middleware
         Request::HEADER_X_FORWARDED_PROTO,
         Request::HEADER_X_FORWARDED_AWS_ELB,
     ];
-    
+
+    public function handle($request, \Closure $next)
+    {
+        if (app()->environment('production')) {
+            $request->server->set('HTTPS', 'on');
+        }
+        return parent::handle($request, $next);
+    }
+
 }
